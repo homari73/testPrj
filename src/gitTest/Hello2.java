46da3846a285403fe0a6b99ff21ef7bde64711bd
homari73 <homari@gmail.com>
2022-05-23 10:07:37 +0900

    2222

--- src/gitTest/Hello2.java
+++ src/gitTest/Hello2.java
@@ -1,10 +1,10 @@
 package gitTest;
 
-public class Hello {
+public class Hello2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello1111~~");
+		System.out.println("Hello2~~~");
 	}
 
 }
